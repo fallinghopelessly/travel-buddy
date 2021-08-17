0ccafc0f3e77ee6ee5daf992561a8bb0f3696370
Kanak <kanakbisht@gmail.com>
2021-08-17 22:51:43 +0530

    added files

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocalActivityOutlined';
+import Rating from './styles';
+
+import useStyles from './styles';
+
+const Map = () => {
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    return(
+        <h1>Map</h1>
+    );
+}
+
+export default Map;
